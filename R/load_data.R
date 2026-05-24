d86d5c0dbe22fec170171db4ff392453e2d4ea5d
Brett Van <brettvan23@gmail.com>
2026-05-23 21:46:01 -0700

    Created new package!

---
+++ R/load_data.R
@@ -0,0 +1,14 @@
+#' Loads the Break Free From Plastic dataset.
+#'
+#' @return A tibble containing plastic waste data.
+#' @importFrom readr read_csv
+#' @export
+#'
+#' @examples
+#' load_data()
+load_data <- function() {
+  readr::read_csv(
+    "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-26/plastics.csv"
+  )
+}
+
